3317e902914abce85dca078198d646483e2add95
shivy54 <59441312+shivy54@users.noreply.github.com>
2021-04-01 00:10:09 +0530

    Add files via upload

---
+++ Screens/ReadStoryScreen.js
@@ -0,0 +1,108 @@
+import * as React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  FlatList,
+} from "react-native";
+import db from "../config";
+import ReadStoryScreen from "./WriteStoryScreen";
+
+export default class ReadStoryScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      AllBooks: [],
+      Searched: [],
+      search: "",
+    };
+  }
+  componentDidMount = async () => {
+    const query = await db.collection("Story");
+    query.docs.map((doc) => {
+      this.setState({
+        AllBooks: [...this.state.AllBooks, doc.data()],
+        Searched: [...this.state.Searched, doc.data()]
+      });
+    });
+  };
+
+  searchBooks = async (text) => {
+    var text = text.toUpperCase();
+    const search = await db
+      .collection("Story")
+      .where("Title", "==", text)
+      .get();
+    Story.docs.map((doc) => {
+      this.setState({
+        Searched: [...this.Searched, doc.data()],
+      });
+    });
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.searchBar}>
+          <TextInput
+            style={styles.bar}
+            placeholder="Enter Book Name"
+            onChangeText={(text) => {
+              this.setState({ search: text });
+            }}
+          />
+
+          <TouchableOpacity
+            style={styles.searchButton}
+            onPress={() => {
+              this.searchBooks(this.state.search);
+            }}
+          >
+            <Text>Search</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <FlatList
+            data={this.state.Searched}
+            renderItem={({ item }) => (
+              <View style={{ borderBottomWidth: 2 }}>
+                <Text>{"Title of book" + item.Title}</Text>
+                <Text>{"Author" + item.Author}</Text>
+              </View>
+            )}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+  searchBar: {
+    flexDirection: "row",
+    height: 40,
+    width: "auto",
+    borderWidth: 0.5,
+    alignItems: "center",
+    backgroundColor: "grey",
+  },
+  bar: {
+    borderWidth: 2,
+    height: 30,
+    width: 300,
+    paddingLeft: 10,
+  },
+  searchButton: {
+    borderWidth: 1,
+    height: 30,
+    width: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "green",
+  },
+});
